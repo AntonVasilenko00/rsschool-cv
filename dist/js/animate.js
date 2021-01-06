f4128f369a2dfafcc88e8143915c91a8b1abf82c
Anton <anton.vasilenko00@gmail.com>
2021-01-07 00:29:02 +0300

    feat: finish full page

---
+++ dist/js/animate.js
@@ -0,0 +1,33 @@
+function animate(className,addClassName) {
+    var elements;
+    var windowHeight;
+    const neededScrollHeight = -535; 
+    function choseElems(name){
+        return document.getElementsByClassName(name);
+    }
+  
+    function init() {
+      elements = choseElems(className);
+      windowHeight = window.innerHeight;
+    }
+  
+    function checkPosition() {
+      for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var positionFromTop = elements[i].getBoundingClientRect().top;
+  
+        if (positionFromTop - windowHeight <= 0) {
+          element.classList.add(addClassName);
+          console.log(addClassName + "  " + positionFromTop + "  " + windowHeight);
+        }
+      }
+    }
+  
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  
+    init();
+    checkPosition();
+  };
+animate("aside__section","aside__section--loaded");
+animate("main__section","main__section--loaded");
