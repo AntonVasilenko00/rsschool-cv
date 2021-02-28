9ba9e5fa1b37aa224296934de2a502aee0697106
Anton <anton.vasilenko00@gmail.com>
2021-02-28 15:37:59 +0300

    fix: minor fixes

--- dist/js/animate.js
+++ dist/js/animate.js
@@ -1,40 +1,33 @@
-$( document ).scrollTop( 0);
-function animate(className,addClassName) {
-    var elements;
-    var windowHeight;
-    const neededScrollHeight = -535; 
-    function choseElems(name){
-        return document.getElementsByClassName(name);
-    }
-  
-    function init() {
-      elements = choseElems(className);
-      windowHeight = window.innerHeight;
-    }
-  
-    function checkPosition() {
-      for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        var positionFromTop = elements[i].getBoundingClientRect().top;
-  
-        if (positionFromTop - windowHeight <= 0) {
-          element.classList.add(addClassName);
-        }
+$(document).scrollTop(0);
+function animate(className, addClassName) {
+  var elements;
+  var windowHeight;
+  const neededScrollHeight = -535;
+  function choseElems(name) {
+    return document.getElementsByClassName(name);
+  }
+
+  function init() {
+    elements = choseElems(className);
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var positionFromTop = elements[i].getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight <= 0) {
+        element.classList.add(addClassName);
       }
     }
-  
-    window.addEventListener('scroll', checkPosition);
-    window.addEventListener('resize', init);
-  
-    init();
-    checkPosition();
-};
-animate("aside__section","aside__section--loaded");
-animate("main__section","main__section--loaded");
+  }
 
-//deactivate save scroll on update
-$(document).ready(function(){
-  setTimeout(function(){
-      window.scrollTo(0, 0);
-  }, 1);
-});
+  window.addEventListener('scroll', checkPosition);
+  window.addEventListener('resize', init);
+
+  init();
+  checkPosition();
+};
+animate("aside__section", "aside__section--loaded");
+animate("main__section", "main__section--loaded");
